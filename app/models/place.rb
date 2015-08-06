dbcb78c8f6899410f5abedfb77606c2b54de1dc5
Mikaila <mwwaters03@gmail.com>
2015-08-06 20:59:44 +0000

    Trying to debug api issue

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,7 +2,7 @@ class Place < ActiveRecord::Base
 	belongs_to :user
 	geocoded_by :address
 	after_validation :geocode
-	
+
 	validates :name, :presence => true, :length => { :minimum => 3}
 	validates :address, :description, :presence => true
 end
