15258c71c39fc86f040223f5d0a07bb0ce7f908f
Mikaila <mwwaters03@gmail.com>
2015-08-09 21:27:09 +0000

    Added comments controller

--- db/migrate/20150809200319_create_comments.rb
+++ db/migrate/20150809200319_create_comments.rb
@@ -1,11 +1,11 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-		t.text :message
-		t.string :rating
+  		t.text :message
+  		t.string :rating
 
-		t.integer :user_id
-		t.integer :place_id
+  		t.integer :user_id
+  		t.integer :place_id
 	    t.timestamps
     end
 
