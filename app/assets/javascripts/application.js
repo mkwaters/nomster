d2bd338e4daca88fe5cdbd37f8db1d9570673afc
Mikaila <mwwaters03@gmail.com>
2015-08-09 22:22:48 +0000

    Built adding comment modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require_tree .
-// Loads all Bootstrap javascripts
+
 //= require bootstrap
+// Loads all Bootstrap javascripts
