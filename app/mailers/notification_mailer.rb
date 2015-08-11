38a300a6ba1f8ca6f1525d7e9fda1e520813130d
Mikaila <mwwaters03@gmail.com>
2015-08-11 22:10:33 +0000

    Added email to send notifications, not actually working yet

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,8 +2,7 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@remotely.com"
 
   def comment_added
-  	mail(tp: "mwwaters03@gmail.com",
+  	mail(to: "mikailawaters@gmail.com",
   		subject: "A comment has been added to your place!")
   end
-end 
-
+end
