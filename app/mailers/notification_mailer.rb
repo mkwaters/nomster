7fc91aa8953153d22f4e96003d73247cc8215041
Mikaila <mwwaters03@gmail.com>
2015-08-11 22:30:55 +0000

    Got mailer to work by changing small stuff in development file

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,12 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@remotely.com"
 
   def comment_added
-  	mail(to: "mikailawaters@gmail.com",
-  		subject: "A comment has been added to your place!")
+  	# (comment)
+  	# @place = comment.place
+  	# @place_owner = @place.user
+
+  	mail(to: "mwwaters03@gmail.com",
+  		# @place_owner.email,
+  		subject: "A comment has been added to your place")
   end
 end
