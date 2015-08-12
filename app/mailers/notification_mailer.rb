2ac281072ec6a5418ba81b3c71ac37197cce619c
Mikaila <mwwaters03@gmail.com>
2015-08-12 13:07:53 +0000

    Adjusted mailer to send to user

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,13 +1,11 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@remotely.com"
 
-  def comment_added
-  	# (comment)
-  	# @place = comment.place
-  	# @place_owner = @place.user
+  def comment_added(comment)
+  	@place = comment.place
+  	@place_owner = @place.user
 
-  	mail(to: "mwwaters03@gmail.com",
-  		# @place_owner.email,
+  	mail(to: @place_owner.email,
   		subject: "A comment has been added to your place")
   end
 end
