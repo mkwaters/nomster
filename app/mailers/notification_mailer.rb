8bc4c76677696d9870cfd6cfe7e84ef15000bf6e
Mikaila <mwwaters03@gmail.com>
2015-08-12 14:05:09 +0000

    Emails now lead to place link and go to any user

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -6,6 +6,6 @@ class NotificationMailer < ActionMailer::Base
   	@place_owner = @place.user
 
   	mail(to: @place_owner.email,
-  		subject: "A comment has been added to your place")
+  		subject: "A comment has been added to #{@place.name}")
   end
 end
